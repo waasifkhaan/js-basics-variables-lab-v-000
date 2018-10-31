53ca039852e956c4bbe1c756558caef587254816
Wasif Khan <waasif.khaan@gmail.com>
2018-10-31 23:31:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
-let mostProfitableNeighborhood;
-mostProfitableNeighborhood = Chelsea; 
-
+let Chelsea;
+Chelsea = mostProfitableNeighborhood;
